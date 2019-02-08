300c9b08a0a3b23bf38c740da6f5e651b19d7f0d
Anirban <canirban99@gmail.com>
2019-02-08 16:09:34 +0100

    first commit

---
+++ src/main/java/com/example/chargingstation/SpringBootChargingStationApplication.java
@@ -0,0 +1,14 @@
+package com.example.chargingstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChargingStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChargingStationApplication.class, args);
+	}
+
+}
+
